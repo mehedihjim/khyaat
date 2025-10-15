c0b9dcd7088ec97c65e3430128cbb93ef4c21195
mhjim <mhjim.info@gmail.com>
2025-10-15 12:55:22 +0600

    migration route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/setup-database', function () {
+    try {
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return 'Database setup complete! Tables created: ' . Artisan::output();
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
